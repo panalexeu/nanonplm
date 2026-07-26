da4754ea9e149cb1cdb64bada1b0e17ce5888d11
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2026-07-26 21:27:58 +0300

    upd sample.py

--- nanonplm/sample.py
+++ nanonplm/sample.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
     model = Model.from_pretrained(ckpt_path)
     tokenizer = BaseTokenizer()
 
-    input = "to be"
+    input = "to be or not to"
     print(input, end="")
     tokens = tokenizer.__call__(input)
     input_ids = torch.tensor([model.vocab_table[t] for t in tokens])
-    out_ids = model.sample(x=input_ids, max_tokens=64, t=1.0, stream=True)
+    out_ids = model.sample(x=input_ids, max_tokens=1024, t=0.7, stream=True)
     vocab_tokens = list(model.vocab_table.keys())
     sampled_tokens = [vocab_tokens[id] for id in out_ids]
 
